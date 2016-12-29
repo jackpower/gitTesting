4530e011538548eedd5cb58b56b811329c3b5411
dataJack88 <jack_power2@hotmail.com>
2016-12-29 19:15:16 +0000

    Start work on determining correlation between predictor variables and target variables. Still WIP.

--- assetClassification.py
+++ assetClassification.py
@@ -9,12 +9,21 @@ Created on Thu Dec 29 17:31:42 2016
 # A script to test if different asset classes respond 
 # differently to different predictor variables
 
-# Load data
-
-import pandas as pd
+# Set working directory
 import os
-
 os.chdir('/Users/Jack/Documents/gitTesting')
+
+# Load data
+import pandas as pd
 
 with pd.HDFStore('data/train.h5') as train:
     df = train.get('train')
+    
+# Sample dataset for testing the script
+sampleAssetIDs = df['id'].drop_duplicates().sample(frac=0.05)
+df = df.loc[df['id'].isin(sampleAssetIDs)]
+del(sampleAssetIDs)
+
+df.groupby(['id']).corr()["y"]
+
+newDF = pd.DataFrame(index=df['id'].drop_duplicates(), columns=df.columns)
